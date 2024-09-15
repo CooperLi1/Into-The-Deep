6aa240566dbcf47aa25c5bb97e6f7f51e8e52a24
Kevin Dong <107311558+kevinndongg@users.noreply.github.com>
2024-09-14 22:10:31 -0400

    pasted sticky gamepad, blulinearopmode
    cleaned up blulinear opmode

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/blucru/opmode/BluLinearOpMode.java
@@ -0,0 +1,116 @@
+package org.firstinspires.ftc.teamcode.blucru.opmode;
+
+import android.util.Log;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.arcrobotics.ftclib.command.CommandScheduler;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.blucru.common.hardware.StickyGamepad;
+import org.firstinspires.ftc.teamcode.blucru.common.util.Alliance;
+import org.firstinspires.ftc.teamcode.blucru.common.util.Globals;
+import org.firstinspires.ftc.teamcode.blucru.common.subsystems.Robot;
+import org.firstinspires.ftc.teamcode.blucru.common.subsystems.drivetrain.Drivetrain;
+
+public abstract class BluLinearOpMode extends LinearOpMode {
+    public Alliance alliance;
+    public Robot robot;
+    public Drivetrain drivetrain;
+
+    public StickyGamepad stickyG1;
+    public StickyGamepad stickyG2;
+
+    ElapsedTime runtime;
+    double lastTime;
+    double loopTimeSum;
+    int loopTimeCount;
+
+    public final void runOpMode() throws InterruptedException {
+        Globals.runtime = new ElapsedTime();
+        CommandScheduler.getInstance().cancelAll();
+        alliance = Globals.alliance;
+        stickyG1 = new StickyGamepad(gamepad1);
+        stickyG2 = new StickyGamepad(gamepad2);
+        robot = Robot.getInstance();
+        robot.create(hardwareMap);
+        Globals.setVoltage(robot.getVoltage());
+        initialize();
+        robot.init();
+        while(opModeInInit()) {
+            stickyG1.update();
+            stickyG2.update();
+            CommandScheduler.getInstance().run();
+            initLoop();
+            try {
+                telemetry();
+            } catch (Exception e) {}
+            telemetry.update();
+        }
+        waitForStart();
+        onStart();
+        Globals.runtime.reset();
+        runtime = new ElapsedTime(); // start timer
+        runtime.reset();
+
+        while (!isStopRequested() && opModeIsActive()) {
+            stickyG1.update();
+            stickyG2.update();
+            robot.read();
+            periodic();
+            CommandScheduler.getInstance().run();
+            robot.write();
+
+            // calculate average loop time
+            loopTimeSum += runtime.milliseconds() - lastTime;
+            lastTime = runtime.milliseconds();
+            loopTimeCount++;
+
+            telemetry();
+            robot.telemetry(telemetry);
+            telemetry.addData("alliance:", alliance);
+            telemetry.addData("Loop Time", loopTimeSum / loopTimeCount);
+            resetLoopTime();
+            telemetry.update();
+        }
+
+        end();
+        Robot.kill();
+        Log.i("BCLinearOpMode", "OpMode Stopped");
+    }
+
+    // methods to be overriden
+    public void initialize() {}
+    public void initLoop() {}
+    public void onStart() {}
+    public void periodic() {}
+    public void telemetry() {}
+    public void end() {}
+
+
+//    public void addOuttake() {outtake = robot.addOuttake();}
+
+
+    /*
+    this method is used to enable the FTC Dashboard telemetry and field overlay
+     */
+    public void enableFTCDashboard() {telemetry = new MultipleTelemetry(FtcDashboard.getInstance().getTelemetry(), telemetry);}
+
+    public double currentSecs() {return runtime.seconds();}
+
+    public double currentTime() {return runtime.milliseconds();}
+
+    public double secsSince(double timeSecs) {
+        return runtime.seconds() - timeSecs;
+    }
+
+    public double timeSince(double timeMillis) {
+        return runtime.milliseconds() - timeMillis;
+    }
+
+    private void resetLoopTime() {
+        loopTimeSum = 0;
+        loopTimeCount = 0;
+    }
+}
