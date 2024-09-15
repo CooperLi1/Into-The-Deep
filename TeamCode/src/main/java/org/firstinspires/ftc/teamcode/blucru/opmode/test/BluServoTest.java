4898b7f10b4cb2cf601ca3f97db7f50662748ed4
Kevin Dong <107311558+kevinndongg@users.noreply.github.com>
2024-09-14 23:00:49 -0400

    created bluservo test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/blucru/opmode/test/BluServoTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/blucru/opmode/test/BluServoTest.java
@@ -1,10 +1,14 @@
 package org.firstinspires.ftc.teamcode.blucru.opmode.test;
 
+import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.blucru.common.hardware.BluServo;
 import org.firstinspires.ftc.teamcode.blucru.common.util.Globals;
 
+@Config
+@TeleOp(name = "Blu servo test", group = "test")
 public class BluServoTest extends LinearOpMode {
     public static double position = 0.5;
     public static String name = "wrist";
