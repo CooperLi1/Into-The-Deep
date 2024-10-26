ef5e58320069694f6992bb73926d6bcb7e104dfc
kevinndongg <kevin.dong.haonan@gmail.com>
2024-10-26 10:25:35 -0400

    renamed drive kinematics

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/blucru/common/subsystems/drivetrain/control/DriveKinematics.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/blucru/common/subsystems/drivetrain/control/DriveKinematics.java
@@ -1,8 +1,14 @@
 package org.firstinspires.ftc.teamcode.blucru.common.subsystems.drivetrain.control;
 
+import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 
-public class Kinematics {
+@Config
+public class DriveKinematics {
+    public static double AXIAL_DECELERATION = 0.5,
+        LATERAL_DECELERATION = 0.5,
+        HEADING_DECELERATION = 0.5;
+
     public static double getHeadingTowardsPoint(Pose2d currentPose, Pose2d targetPose) {
         return Math.atan2(targetPose.getY() - currentPose.getY(),
                             targetPose.getX() - currentPose.getX());
@@ -17,5 +23,9 @@ public class Kinematics {
 
         // derive atan(yDelta / xDelta) with respect to time
         return (xDelta * yVel - yDelta * xVel) / (xDelta * xDelta + yDelta * yDelta);
+    }
+
+    public static Pose2d getStopPose(Pose2d currentPose, Pose2d currentVelocity) {
+        return new Pose2d();
     }
 }
