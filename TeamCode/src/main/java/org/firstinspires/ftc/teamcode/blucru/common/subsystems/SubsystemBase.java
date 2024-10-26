84a4e385dc21f11e7de020105797ccb741cd4ffa
kevinndongg <kevin.dong.haonan@gmail.com>
2024-10-25 23:48:29 -0400

    created subsystem base

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/blucru/common/subsystems/SubsystemBase.java
@@ -0,0 +1,30 @@
+package org.firstinspires.ftc.teamcode.blucru.common.subsystems;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.teamcode.blucru.common.hardware.BluHardwareDevice;
+
+import java.util.ArrayList;
+
+public abstract class SubsystemBase implements Subsystem {
+    ArrayList<BluHardwareDevice> devices;
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void read() {
+
+    }
+
+    @Override
+    public void write() {
+
+    }
+
+    @Override
+    public void telemetry(Telemetry telemetry) {
+
+    }
+}
