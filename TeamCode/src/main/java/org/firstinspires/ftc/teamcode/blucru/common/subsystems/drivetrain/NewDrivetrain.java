e3a82ed25c0c1f2a6a52e99e15b90ff5a753fa3a
Kevin Dong <107311558+kevinndongg@users.noreply.github.com>
2024-11-14 08:13:30 -0500

    started new dt

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/blucru/common/subsystems/drivetrain/NewDrivetrain.java
@@ -0,0 +1,34 @@
+package org.firstinspires.ftc.teamcode.blucru.common.subsystems.drivetrain;
+
+import com.arcrobotics.ftclib.command.Subsystem;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.teamcode.blucru.common.subsystems.BluSubsystem;
+import org.firstinspires.ftc.teamcode.roadrunner.drive.SampleMecanumDrive;
+
+public class NewDrivetrain extends SampleMecanumDrive implements BluSubsystem, Subsystem {
+    public NewDrivetrain(HardwareMap hwMap) {
+        super(hwMap);
+    }
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void read() {
+
+    }
+
+    @Override
+    public void write() {
+
+    }
+
+    @Override
+    public void telemetry(Telemetry telemetry) {
+
+    }
+}
